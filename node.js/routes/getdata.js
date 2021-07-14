12c0bd554fccb1c05c1fa20a82c24ff864465ec1
102631479 <78398324+102631479@users.noreply.github.com>
2021-07-14 13:57:17 +0800

    爬虫

--- node.js/routes/getdata.js
+++ node.js/routes/getdata.js
@@ -22,37 +22,39 @@ Referer: https://servicewechat.com/wxded25a5479be2a8c/129/page-frame.html
 
 
 router.get('/getdata', (req, res) => {
-    superagent.post('pay.elanvm.com')
+    superagent.post('https://app.dennis.cn/v1/wxmall/advanceFix/afIndex')
         .set({
-            "Host": "pay.elanvm.com",
-            "Connection": "keep-alive",
-            "Content-Length": "0",
-            "mobile": "16237187592",
+            // "Host": "",
+            // "Connection": "keep-alive",
+            // "Accept-Encoding": "gzip, deflate, br",
+            'username': 'dns_ee7190d9fcfd',
             "content-type": "application/x-www-form-urlencoded",
             "type": '1',
-            "token": "845194cc69d8fddf1841d7c43bb2aa755fa5fe106344f806cdb275dad4838a9b",
+            "token": "de16dbd357c2709f853e4102a34f77b33350e03900ab03e6c86cd17111ab3ca6",
             'User-Agent': " Mozilla/5.0 (iPhone; CPU iPhone OS 12_5_3 like Mac OS X)",
-            "Accept-Encoding": "gzip,compress,br,deflate",
-            "Referer": " https://servicewechat.com/wxded25a5479be2a8c/129/page-frame.html"
+        })
+        .send({
+            mobile:"18300692207",
+            apId:"5e40d21229274cd7a11c871b46bdc5f6"
         })
         .end(function (err, sres) {
             console.log(err, 'err');
             console.log(sres, 'sres');
-            res.send(items);
-            if (err) {
-                // return next(err);
-            }
-            var $ = cheerio.load(sres.text);
-            var items = [];
-            $('#topic_list .topic_title').each(function (idx, element) {
-                // console.log(element,'element');
-                // console.log(idx,'idx');
-                var $element = $(element);
-                items.push({
-                    title: $element.attr('title'),
-                    href: $element.attr('href')
-                });
-            });
+            res.send(sres.body);
+            // if (err) {
+            //     // return next(err);
+            // }
+            // var $ = cheerio.load(sres.text);
+            // var items = [];
+            // $('#topic_list .topic_title').each(function (idx, element) {
+            //     // console.log(element,'element');
+            //     // console.log(idx,'idx');
+            //     var $element = $(element);
+            //     items.push({
+            //         title: $element.attr('title'),
+            //         href: $element.attr('href')
+            //     });
+            // });
 
         });
 })
